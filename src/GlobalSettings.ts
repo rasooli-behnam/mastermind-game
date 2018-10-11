ada18608108311e96e461d24db0c7150f57a81c5
Behnam Rasooli <ben.rasooli@gmail.com>
2018-10-12 00:03:52 +1100

    Add GlobalSettings
    It contains view dimentions.

---
+++ src/GlobalSettings.ts
@@ -0,0 +1,10 @@
+interface GlobalSettingsType {
+  view: {
+    width: number;
+    height: number;
+  };
+}
+
+export const GlobalSettings: GlobalSettingsType = {
+  view: { width: 400, height: 600 }
+};
