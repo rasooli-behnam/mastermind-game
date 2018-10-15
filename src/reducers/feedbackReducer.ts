3ece328b5cdbb9ab5b350ae902a4453900f1957e
Behnam <rasooli.behnam@yahoo.com>
2018-10-15 14:15:31 +1100

    Update feedbackReducer to calculate the whole feedback

--- src/reducers/feedbackReducer.ts
+++ src/reducers/feedbackReducer.ts
@@ -4,12 +4,13 @@ import { GetFeedbackAction } from "src/actions";
 const cloneDeep = require("lodash.clonedeep");
 
 interface FeedbackForIndividualSet {
+  range: number[];
   rightGuesses: number;
   rightGuessesWithWrongSpot: number;
 }
 
 export interface Feedback {
-  guesses: string[];
+  clonedGuesses: string[];
   overallSet: FeedbackForIndividualSet;
   firstSubSet: FeedbackForIndividualSet;
   secondSubset: FeedbackForIndividualSet;
@@ -20,16 +21,39 @@ export default function feedbackReducer(
   action: GetFeedbackAction
 ) {
   if (action.type === constants.GET_FEEDBACK) {
-    const feedback = defaultFeedback;
+    const feedback: Feedback = cloneDeep(defaultFeedback);
 
-    const guesses = [...action.payload.guesses];
-    const combination = [...action.payload.combination];
+    const guesses = action.payload.guesses;
+    const combination = action.payload.combination;
 
-    for (let i = 0; i < guesses.length; i++) {
-      if (guesses[i] === combination[i]) {
-        feedback.overallSet.rightGuesses++;
-      }
-    }
+    const overallSetFeedback = getFeedbackFor(guesses, combination);
+    const firstSubSetFeedback = getFeedbackFor(
+      guesses.slice(
+        feedback.firstSubSet.range[0],
+        feedback.firstSubSet.range[1]
+      ),
+      combination.slice(
+        feedback.firstSubSet.range[0],
+        feedback.firstSubSet.range[1]
+      )
+    );
+    const secondSubsetFeedback = getFeedbackFor(
+      guesses.slice(
+        feedback.secondSubset.range[0],
+        feedback.secondSubset.range[1]
+      ),
+      combination.slice(
+        feedback.secondSubset.range[0],
+        feedback.secondSubset.range[1]
+      )
+    );
+
+    feedback.overallSet = { ...feedback.overallSet, ...overallSetFeedback };
+    feedback.firstSubSet = { ...feedback.firstSubSet, ...firstSubSetFeedback };
+    feedback.secondSubset = {
+      ...feedback.secondSubset,
+      ...secondSubsetFeedback
+    };
 
     const feedbacks = cloneDeep(prevState);
     feedbacks.push(feedback);
@@ -40,17 +64,54 @@ export default function feedbackReducer(
   return prevState;
 }
 
+function getFeedbackFor(
+  guesses: string[],
+  combination: string[]
+): { rightGuesses: number; rightGuessesWithWrongSpot: number } {
+  const result = {
+    rightGuesses: 0,
+    rightGuessesWithWrongSpot: 0
+  };
+  const clonedGuesses: string[] = cloneDeep(guesses);
+  const clonedCombination: string[] = cloneDeep(combination);
+
+  for (let i = 0; i < clonedGuesses.length; i++) {
+    if (clonedGuesses[i] === clonedCombination[i]) {
+      clonedGuesses[i] = "used";
+      clonedCombination[i] = "used";
+      result.rightGuesses++;
+    }
+  }
+
+  for (let i = 0; i < clonedGuesses.length; i++) {
+    if (clonedGuesses[i] != "used") {
+      for (let ii = 0; ii <= clonedCombination.length; ii++) {
+        if (clonedGuesses[i] === clonedCombination[ii]) {
+          clonedCombination[ii] = "used";
+          result.rightGuessesWithWrongSpot++;
+          break;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
 const defaultFeedback: Feedback = {
-  guesses: [],
+  clonedGuesses: [],
   overallSet: {
+    range: [0, 8],
     rightGuesses: 0,
     rightGuessesWithWrongSpot: 0
   },
   firstSubSet: {
+    range: [1, 5],
     rightGuesses: 0,
     rightGuessesWithWrongSpot: 0
   },
   secondSubset: {
+    range: [3, 7],
     rightGuesses: 0,
     rightGuessesWithWrongSpot: 0
   }
